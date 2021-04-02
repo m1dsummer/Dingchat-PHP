b58812fdac841c68cad7355ae076eb74f5c5c2f0
abel <cafwind@outlook.com>
2021-04-02 23:22:01 +0800

    修复部署工具bug

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 # compile the frontend code and copy them to backend/ folder
 
-cd frontend
-
+PROJECT_ROOT=$(dirname $(readlink -f "$0"))
+cd "$PROJECT_ROOT/frontend"
+yarn
 yarn build
+rm -rf ../backend/js
+cp -r dist/js ../backend
+rm -rf ../backend/css
+cp -r dist/css ../backend
+cp dist/index.html ../backend
